2031e2ccfc1ad6fdc68a4b217c46eb1edcb60ee6
urbingt <urbinggt@mail.com>
2016-10-07 10:57:43 -0600

    so much work to do

---
+++ node_modules/angular-on-screen-keyboard/js/angular-on-screen-keyboard.js
@@ -0,0 +1,174 @@
+/* global angular:false */
+angular.module('onScreenKeyboard', ['ngSanitize'])
+    .directive('onScreenKeyboard', function ($timeout, $document) {
+      'use strict';
+      
+        return {
+            restrict: 'E',
+            bindToController: true,
+            controllerAs: 'ctrl',
+            scope: {
+                rows : '=?',
+                uppercaseAllWords : '@',
+            },
+            controller: function($sce){
+                var ctrl = this;
+
+                if (!ctrl.rows){
+                    ctrl.rows = [
+                        ['1', '2', '3', '4','5','6','7','8', '9', '0', {type: 'erase', colspan: 2, text: '&lArr;'}],
+                        ['q','w','e','r','t','y','u','i','o','p','@'],
+                        ['a','s','d','f','g','h','j','k','l','-','_', {type: 'margin'}],
+                        [{type: 'shift', upperCase: '&dArr;', lowerCase: '&uArr;'}, 'z','x','c','v','b','n','m',',', '.',{type: 'shift', upperCase: '&dArr;', lowerCase: '&uArr;'}],
+                        [{type: 'margin'}, {type: 'space', colspan: 9, text: ' '}]
+                    ];
+                }
+
+                ctrl.getText = function(key){
+                    if (key.type === 'margin')
+                        return '';
+
+                    var val = '';
+
+                    if (key.text)
+                        val = key.text;
+                    else  if (key.lowerCase && !ctrl.isUpperCase)
+                        val = key.lowerCase;
+                    else  if (key.upperCase && ctrl.isUpperCase)
+                        val = key.upperCase;
+                    else {
+                        val = ctrl.isUpperCase ? key.toUpperCase() : key.toLowerCase();
+                    }
+
+                    if (val && val.indexOf('&') > -1)
+                        return $sce.trustAsHtml(val);
+
+                    return val;
+                };
+            },
+            link: function (scope, element, attr) {
+                var ctrl = scope.ctrl;
+                ctrl.isUpperCase = false;
+                ctrl.lastInputCtrl = null;
+                ctrl.startPos = null;
+                ctrl.endPos = null;
+
+                ctrl.printKeyStroke = function(key, event){
+                    if (!ctrl.lastInputCtrl)
+                        return;
+
+                    ctrl.startPos = ctrl.lastInputCtrl.selectionStart;
+                    ctrl.endPos = ctrl.lastInputCtrl.selectionEnd;
+
+                    if (key.type === 'erase'){
+                        ctrl.eraseKeyStroke();
+                        return;
+                    } else if (key.type === 'shift'){
+                        ctrl.isUpperCase = !ctrl.isUpperCase;
+                        return;
+                    }
+
+                    var htmlKeyVal = angular.element(event.target || event.srcElement).text();
+                    var lastInputCtrl = angular.element(ctrl.lastInputCtrl);
+                    var val = lastInputCtrl.val();
+                    var pre = val.substring(0, ctrl.startPos);
+                    var post = val.substring(ctrl.endPos, val.length);
+                    lastInputCtrl.val(pre + htmlKeyVal + post);
+                    lastInputCtrl.triggerHandler('change');
+
+                    ctrl.startPos += htmlKeyVal.length;
+                    ctrl.endPos += htmlKeyVal.length;
+                    ctrl.lastInputCtrl.selectionStart = ctrl.startPos;
+                    ctrl.lastInputCtrl.selectionEnd = ctrl.startPos;
+                    ctrl.setKeyboardLayout();
+                };
+
+                ctrl.refocus = function () {
+                    ctrl.lastInputCtrl.focus();
+                };
+
+                ctrl.eraseKeyStroke = function () {
+                    if (!ctrl.lastInputCtrl)
+                        return;
+
+                    var hasSel = ctrl.startPos !== ctrl.endPos;
+
+                    var lastInputCtrl = angular.element(ctrl.lastInputCtrl);
+                    var val = lastInputCtrl.val();
+                    var pre = val.substring(0, hasSel ? ctrl.startPos : ctrl.startPos - 1);
+                    var post = val.substring(ctrl.endPos, val.length);
+
+                    lastInputCtrl.val(pre + post);
+                    lastInputCtrl.triggerHandler('change');
+
+                    if (hasSel) {
+                        ctrl.endPos = ctrl.startPos;
+                    }
+                    else {
+                        ctrl.startPos--;
+                        ctrl.endPos--;
+                    }
+                    ctrl.lastInputCtrl.selectionStart = ctrl.startPos;
+                    ctrl.lastInputCtrl.selectionEnd = ctrl.startPos;
+                    ctrl.setKeyboardLayout();
+                    ctrl.refocus();
+                };
+
+                ctrl.setKeyboardLayout = function () {
+                    if (!ctrl.lastInputCtrl){
+                        ctrl.isUpperCase = true;
+                        return;
+                    }
+                    else if (ctrl.lastInputCtrl.className && ctrl.isUpperCase)
+                        ctrl.isUpperCase = false;
+                    else if (angular.element(ctrl.lastInputCtrl).val().length === 0) {
+                        ctrl.isUpperCase = true;
+                    }
+                    else if (angular.element(ctrl.lastInputCtrl).val().slice(-1) === ' ' && !ctrl.isUpperCase && attr.uppercaseAllWords !== undefined)
+                        ctrl.isUpperCase = true;
+                    else{
+                        ctrl.isUpperCase = false;
+                    }
+                };
+
+                var focusin = function(event){
+                    var e = event.target || event.srcElement;
+
+                    if (e.tagName === 'INPUT' || e.tagName === 'TEXTAREA') {
+                        ctrl.lastInputCtrl = e;
+                        ctrl.setKeyboardLayout();
+                        scope.$digest();
+                    }
+                };
+
+                var keyup = function(){
+                    if (!ctrl.lastInputCtrl)
+                        return;
+
+                    ctrl.startPos = ctrl.lastInputCtrl.selectionStart;
+                    ctrl.endPos = ctrl.lastInputCtrl.selectionEnd;
+
+                    ctrl.setKeyboardLayout();
+                    scope.$digest();
+                };
+
+                $document.bind('focusin', focusin);
+                $document.bind('keyup', keyup);
+
+                scope.$on("$destroy", function() {
+                    $document.unbind('focusin', focusin);
+                    $document.unbind('keyup', keyup);
+                });
+
+                element.bind('contextmenu', function (event) {
+                    event.preventDefault();
+                    return false;
+                });
+
+                $timeout(function(){
+                    ctrl.isUpperCase = true;
+                },0);
+            },
+            templateUrl: '/templates/angular-on-screen-keyboard.html'
+        };
+    });
---
+++ public/keyboard/module.keyboard.js
@@ -0,0 +1,411 @@
+angular.module('module.keyboard', ['ngSanitize'])
+    .controller('controller.keyboard', function() {
+        console.info('controller.keyboard loaded');
+
+        var keyboard = this;
+
+        keyboard.keys = [{
+                name: "backspace",
+                symbol: "",
+                code: 8,
+            }, {
+                name: "tab",
+                symbol: "",
+                code: 9,
+            }, {
+                name: "enter",
+                symbol: "",
+                code: 13,
+            }, {
+                name: "shift",
+                symbol: "",
+                code: 16,
+            }, {
+                name: "ctrl",
+                symbol: "",
+                code: 17,
+            }, {
+                name: "alt",
+                symbol: "",
+                code: 18,
+            }, {
+                name: "pause",
+                symbol: "",
+                code: 19,
+            }, {
+                name: "break",
+                symbol: "",
+                code: 19,
+            }, {
+                name: "caps-lock",
+                symbol: "",
+                code: 20,
+            }, {
+                name: "escape",
+                symbol: "",
+                code: 27,
+            }, {
+                name: "page-up",
+                symbol: "",
+                code: 33,
+            }, {
+                name: "page-down",
+                symbol: "",
+                code: 34,
+            }, {
+                name: "end",
+                symbol: "",
+                code: 35,
+            }, {
+                name: "home",
+                symbol: "",
+                code: 36,
+            }, {
+                name: "left-arrow",
+                symbol: "&ltrif;",
+                code: 37,
+            }, {
+                name: "up-arrow",
+                symbol: "&utrif;",
+                code: 38,
+            }, {
+                name: "right-arrow",
+                symbol: "&rtrif;",
+                code: 39,
+            }, {
+                name: "down-arrow",
+                symbol: "&dtrif;",
+                code: 40,
+            }, {
+                name: "insert",
+                symbol: "",
+                code: 45,
+            }, {
+                name: "delete",
+                symbol: "",
+                code: 46,
+            }, {
+                name: "0",
+                symbol: "",
+                code: 48,
+            }, {
+                name: "1",
+                symbol: "",
+                code: 49,
+            }, {
+                name: "2",
+                symbol: "",
+                code: 50,
+            }, {
+                name: "3",
+                symbol: "",
+                code: 51,
+            }, {
+                name: "4",
+                symbol: "",
+                code: 52,
+            }, {
+                name: "5",
+                symbol: "",
+                code: 53,
+            }, {
+                name: "6",
+                symbol: "",
+                code: 54,
+            }, {
+                name: "7",
+                symbol: "",
+                code: 55,
+            }, {
+                name: "8",
+                symbol: "",
+                code: 56,
+            }, {
+                name: "9",
+                symbol: "",
+                code: 57,
+            }, {
+                name: "a",
+                symbol: "",
+                code: 65,
+            }, {
+                name: "b",
+                symbol: "",
+                code: 66,
+            }, {
+                name: "c",
+                symbol: "",
+                code: 67,
+            }, {
+                name: "d",
+                symbol: "",
+                code: 68,
+            }, {
+                name: "e",
+                symbol: "",
+                code: 69,
+            }, {
+                name: "f",
+                symbol: "",
+                code: 70,
+            }, {
+                name: "g",
+                symbol: "",
+                code: 71,
+            }, {
+                name: "h",
+                symbol: "",
+                code: 72,
+            }, {
+                name: "i",
+                symbol: "",
+                code: 73,
+            }, {
+                name: "j",
+                symbol: "",
+                code: 74,
+            }, {
+                name: "k",
+                symbol: "",
+                code: 75,
+            }, {
+                name: "l",
+                symbol: "",
+                code: 76,
+            }, {
+                name: "m",
+                symbol: "",
+                code: 77,
+            }, {
+                name: "n",
+                symbol: "",
+                code: 78,
+            }, {
+                name: "o",
+                symbol: "",
+                code: 79,
+            }, {
+                name: "p",
+                symbol: "",
+                code: 80,
+            }, {
+                name: "q",
+                symbol: "",
+                code: 81,
+            }, {
+                name: "r",
+                symbol: "",
+                code: 82,
+            }, {
+                name: "s",
+                symbol: "",
+                code: 83,
+            }, {
+                name: "t",
+                symbol: "",
+                code: 84,
+            }, {
+                name: "u",
+                symbol: "",
+                code: 85,
+            }, {
+                name: "v",
+                symbol: "",
+                code: 86,
+            }, {
+                name: "w",
+                symbol: "",
+                code: 87,
+            }, {
+                name: "x",
+                symbol: "",
+                code: 88,
+            }, {
+                name: "y",
+                symbol: "",
+                code: 89,
+            }, {
+                name: "z",
+                symbol: "",
+                code: 90,
+            }, {
+                name: "left-win",
+                symbol: "",
+                code: 91,
+            }, {
+                name: "right-win",
+                symbol: "",
+                code: 92,
+            }, {
+                name: "select-key",
+                symbol: "",
+                code: 93,
+            },
+            // {
+            //     name: "numpad-0",
+            //     symbol: "",
+            //     code: 96,
+            // }, {
+            //     name: "numpad-1",
+            //     symbol: "",
+            //     code: 97,
+            // }, {
+            //     name: "numpad-2",
+            //     symbol: "",
+            //     code: 98,
+            // }, {
+            //     name: "numpad-3",
+            //     symbol: "",
+            //     code: 99,
+            // }, {
+            //     name: "numpad-4",
+            //     symbol: "",
+            //     code: 100,
+            // }, {
+            //     name: "numpad-5",
+            //     symbol: "",
+            //     code: 101,
+            // }, {
+            //     name: "numpad-6",
+            //     symbol: "",
+            //     code: 102,
+            // }, {
+            //     name: "numpad-7",
+            //     symbol: "",
+            //     code: 103,
+            // }, {
+            //     name: "numpad-8",
+            //     symbol: "",
+            //     code: 104,
+            // }, {
+            //     name: "numpad-9",
+            //     symbol: "",
+            //     code: 105,
+            // }, 
+            // {
+            //     name: "multiply",
+            //     symbol: "",
+            //     code: 106,
+            // }, 
+            // {
+            //     name: "add",
+            //     symbol: "",
+            //     code: 107,
+            // }, {
+            //     name: "subtract",
+            //     symbol: "",
+            //     code: 109,
+            // },
+            // {
+            //     name: "decimal point",
+            //     symbol: "",
+            //     code: 110,
+            // }, 
+            // {
+            //     name: "divide",
+            //     symbol: "",
+            //     code: 111,
+            // }, {
+            //     name: "f1",
+            //     symbol: "",
+            //     code: 112,
+            // }, {
+            //     name: "f2",
+            //     symbol: "",
+            //     code: 113,
+            // }, {
+            //     name: "f3",
+            //     symbol: "",
+            //     code: 114,
+            // }, {
+            //     name: "f4",
+            //     symbol: "",
+            //     code: 115,
+            // }, {
+            //     name: "f5",
+            //     symbol: "",
+            //     code: 116,
+            // }, {
+            //     name: "f6",
+            //     symbol: "",
+            //     code: 117,
+            // }, {
+            //     name: "f7",
+            //     symbol: "",
+            //     code: 118,
+            // }, {
+            //     name: "f8",
+            //     symbol: "",
+            //     code: 119,
+            // }, {
+            //     name: "f9",
+            //     symbol: "",
+            //     code: 120,
+            // }, {
+            //     name: "f10",
+            //     symbol: "",
+            //     code: 121,
+            // }, {
+            //     name: "f11",
+            //     symbol: "",
+            //     code: 122,
+            // }, {
+            //     name: "f12",
+            //     symbol: "",
+            //     code: 123,
+            // }, {
+            //     name: "num-lock",
+            //     symbol: "",
+            //     code: 144,
+            // }, {
+            //     name: "scroll-lock",
+            //     symbol: "",
+            //     code: 145,
+            // }, 
+            {
+                name: "semi-colon",
+                symbol: "&semi;",
+                code: 186,
+            }, {
+                name: "equal-sign",
+                symbol: "&equals;",
+                code: 187,
+            }, {
+                name: "comma",
+                symbol: "&comma;",
+                code: 188,
+            }, {
+                name: "dash",
+                symbol: "&macr;",
+                code: 189,
+            }, {
+                name: "period",
+                symbol: "&period;",
+                code: 190,
+            }, {
+                name: "forward-slash",
+                symbol: "&sol;",
+                code: 191,
+            }, {
+                name: "grave",
+                symbol: "&grave;",
+                code: 192,
+            }, {
+                name: "open-bracket",
+                symbol: "&lsqb;",
+                code: 219,
+            }, {
+                name: "back-slash",
+                symbol: "&bsol;",
+                code: 220,
+            }, {
+                name: "close-braket",
+                symbol: "&rsqb;",
+                code: 221,
+            }, {
+                name: "single-quote",
+                symbol: "&apos;",
+                code: 222
+            }
+        ];
+    });
